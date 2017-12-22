98207b9ba9b7ff859685aca431749103d766a069
geonyoungkim <c2619zz@naver.com>
2017-12-22 17:54:50 +0900

    jackson error success

--- src/main/java/skuniv/ac/kr/contoller/QnAController.java
+++ src/main/java/skuniv/ac/kr/contoller/QnAController.java
@@ -4,12 +4,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import javax.annotation.Resource;
 
 import model.QnAVO;
 import skuniv.ac.kr.service.QnAService;
